aac6f064c2cbabee0de4c73b660be95a66b78a99
Hadley Wickham <h.wickham@gmail.com>
2026-04-09 14:56:06 -0500

    Simplify code

--- 0-import.R
+++ 0-import.R
@@ -1,4 +1,5 @@
 library(nanoparquet)
+library(purrr)
 
 financing_schemes <- read.csv("raw-data/financing_schemes.csv")
 health_spending <- read.csv("raw-data/health_spending.csv")
@@ -14,21 +15,31 @@ countries <- countries[order(countries$country_name), ]
 write_parquet(countries, "countries.parquet")
 
 # Extract short codes from indicator_code and drop redundant columns
-extract_codes <- function(df) {
-  df$unit <- ifelse(grepl("_che$", df$indicator_code), "che", "usd2023")
-  df$indicator_code <- sub("_.*", "", df$indicator_code)
+extract_codes <- function(df, label_col) {
+  parts <- strsplit(df$indicator_code, "_")
+  df$indicator_code <- map_chr(parts, 1)
+  df$unit <- map_chr(parts, 2)
   df$country_name <- NULL
+  df[[label_col]] <- NULL
   df
 }
 
-health_spending <- extract_codes(health_spending)
+unit_levels <- c("che", "usd2023")
+
+health_spending <- extract_codes(health_spending, "expenditure_type")
 names(health_spending)[names(health_spending) == "indicator_code"] <- "expenditure_type"
+health_spending$expenditure_type <- factor(health_spending$expenditure_type, c("che", "gghed", "pvtd", "ext"))
+health_spending$unit <- factor(health_spending$unit, unit_levels)
 
-financing_schemes <- extract_codes(financing_schemes)
+financing_schemes <- extract_codes(financing_schemes, "financing_scheme")
 names(financing_schemes)[names(financing_schemes) == "indicator_code"] <- "financing_scheme"
+financing_schemes$financing_scheme <- factor(financing_schemes$financing_scheme, c("hf1", "hf2", "hf3", "hf4", "hfnec"))
+financing_schemes$unit <- factor(financing_schemes$unit, unit_levels)
 
-spending_purpose <- extract_codes(spending_purpose)
+spending_purpose <- extract_codes(spending_purpose, "spending_purpose")
 names(spending_purpose)[names(spending_purpose) == "indicator_code"] <- "spending_purpose"
+spending_purpose$spending_purpose <- factor(spending_purpose$spending_purpose, c("hc1", "hc2", "hc3", "hc4", "hc5", "hc6", "hc7", "hc9"))
+spending_purpose$unit <- factor(spending_purpose$unit, unit_levels)
 
 write_parquet(health_spending, "health_spending.parquet")
 write_parquet(financing_schemes, "financing_schemes.parquet")
